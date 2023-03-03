bd116a3bb2f7eb1b5919567992fe7e3329f203a9
Pedro Graeff <pedrocgraeff@gmail.com>
2023-03-03 16:10:36 -0300

    new file:   study.py     class vector created     defined Initialization     started vector adding function

---
+++ study.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Vector():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def add(self, other):
+
+        f
+
+
+a = Vector(2, 3, 4)
+b = Vector(7, 6, 5)
