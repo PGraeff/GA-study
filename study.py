6f8a42574c9dec092faf49e7614188202b2ed3bf
Pedro Graeff <pedrocgraeff@gmail.com>
2023-03-04 15:39:10 -0300

    finished addition function     changed the name to scalar product of two vectors     changed it to be a numpy dot product

--- study.py
+++ study.py
@@ -2,19 +2,17 @@ import numpy as np
 
 class Vector():
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
         self.vectorobj = [x, y, z]    
 
 
     # function to add vectors (acording to GA-Analitica)
-    def add(self, other):
-        self.addedvector = []
-        for i, t in self.vectorobj, other:
-            elem =  t * i
-            self.addedvector.append(elem)
+    def scalar_product(self, other):
+        self.otherobj = [other]
+        self.addedvector = np.dot(self.vectorobj, other)
         return self.addedvector
+    
+    
+
 
 
 a = Vector(2, 3, 4)
