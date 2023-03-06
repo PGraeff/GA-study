8bf3bfcfe01006a3a0afd78fd2ed9aab49530de8
PGraeff <pedrocgraeff@gmail.com>
2023-03-05 22:15:52 -0300

    Created a adding function to the vector class     now you are able to add the vector object in question, with another one, or a list of vectors

--- study.py
+++ study.py
@@ -4,19 +4,26 @@ class Vector():
     def __init__(self, x, y, z):
         self.vectorobj = [x, y, z]    
 
+    # Vector addition, returns a new vector that is the some of the object and an external vector
+    def add(self, others):
+        self.other = [others]
+        self.vector_dict = {i :vector for i, vector in zip(range(len(self.other)), self.other)}
+        for vector in self.vector_dict.values():
+            self.added_vector = [(elem1 + elem2) for elem1, elem2 in zip(vector, self.vectorobj)]
+        return self.added_vector
+
     # function to add vectors (acording to GA-Analitica)
     def scalar_product(self, other):
         self.otherobj = [other]
-        self.addedvector = np.dot(self.vectorobj, other)
-        return self.addedvector
+        self.scalar_value = np.dot(self.vectorobj, other)
+        return self.scalar_value
 
     #checks for linear dependencies between the vectors, if 
     # there are more than two vectors, then it expects a list containing two vectors
     def linear_dependecy(self, others):
         self.others = others    
         if len(others) > 1:
-            self.others = others
-            vector_dict = {i :vector for i, vector in zip(len(others), others)}
+            self.vector_dict = {i :vector for i, vector in zip((len(others)), others)}
 
         # for vector in self.others:
         
@@ -35,4 +42,5 @@ class Vector():
 
 a = Vector(2, 3, 4)
 b = Vector(7, 6, 5)
-# print(a.vectorobj.shape())
+
+print(a.add(b.vectorobj))
