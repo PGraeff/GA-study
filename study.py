b7bdd799badfc40fab41be7b587b4c296cf37791
PGraeff <pedrocgraeff@gmail.com>
2023-03-05 22:41:29 -0300

    implemented the get norm funtion and deleted the check for 	linear dependecy, i will look into it in the future, its too 	complicated for now

--- study.py
+++ study.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math as mt
 
 class Vector():
     def __init__(self, x, y, z):
@@ -17,30 +18,18 @@ class Vector():
         self.otherobj = [other]
         self.scalar_value = np.dot(self.vectorobj, other)
         return self.scalar_value
-
-    #checks for linear dependencies between the vectors, if 
-    # there are more than two vectors, then it expects a list containing two vectors
-    def linear_dependecy(self, others):
-        self.others = others    
-        if len(others) > 1:
-            self.vector_dict = {i :vector for i, vector in zip((len(others)), others)}
-
-        # for vector in self.others:
-        
-
-
-        pass 
-    
-    # multiply two vectors 
-    def vector_multiplication(self, other):
-        pass    
-
+ 
     # gets the norm of the vector
     def get_norm(self): 
-        pass
+        self.temp = []
+        for element in self.vectorobj:
+            self.temp.append(element ** 2)
+        self.norm = mt.sqrt(sum(self.temp))
+        return  self.norm
 
 
 a = Vector(2, 3, 4)
 b = Vector(7, 6, 5)
 
 print(a.add(b.vectorobj))
+print(a.get_norm())
